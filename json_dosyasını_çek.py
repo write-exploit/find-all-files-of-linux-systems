03859c5a843eafb3d1af9f4c4cbad053576e7a04
write-exploit <149171434+write-exploit@users.noreply.github.com>
2023-11-14 14:15:59 +0300

    Update json_dosyasını_çek.py

--- json_dosyasını_çek.py
+++ json_dosyasını_çek.py
@@ -4,4 +4,4 @@ json_dosyasi = open("/home/a/Desktop/dosya.json","r",encoding="utf8")
 
 dosya = json.load(json_dosyasi)
 
-print(dosya[/home/a/Desktop/gizli_dosya])
+print(dosya["/home/a/Desktop/gizli_dosya"])
